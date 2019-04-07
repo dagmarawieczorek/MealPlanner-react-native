34369f42aa9a80369eef5ccb7a23f070373d92cf
dagmarawieczorek <dagmara.sylwia.w@gmail.com>
2019-04-07 02:22:33 +0200

    init

---
+++ components/CalendarView.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+
+
+export class CalendarView extends React.Component {
+  render() {
+    return <ScrollView>
+            <Text>
+            Chocolate bar bonbon lemon drops icing fruitcake tart danish apple pie. Sweet roll jelly beans tart cheesecake chocolate cake wafer cookie. Cake candy canes liquorice. Donut icing chocolate bear claw. Cookie muffin cheesecake muffin jelly-o soufflé cheesecake icing dragée. Gummi bears chocolate cake marzipan marshmallow biscuit bear claw jelly beans. Lemon drops cheesecake powder pudding powder halvah cake apple pie. Tiramisu donut pastry dessert caramels. Candy canes chocolate cake jelly-o. Gummi bears jelly beans pie cotton candy fruitcake cupcake pastry toffee icing. Icing jujubes apple pie bear claw pastry oat cake sugar plum powder macaroon. Topping sugar plum cotton candy gummi bears chocolate. Donut biscuit cookie.
+        </Text>
+    </ScrollView>;
+  }
+}
+
+const styles=StyleSheet.create({
+calendar:{
+    flex:1
+}
+
+})
